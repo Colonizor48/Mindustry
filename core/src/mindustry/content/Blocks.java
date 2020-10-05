4efb4d834abf20348bae86dfcbe6a0261831dfcf
Colonizor48 <43299519+Colonizor48@users.noreply.github.com>
2020-10-05 14:17:04 -0400

    make block forge and related items not hidden

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -2072,21 +2072,21 @@ public class Blocks implements ContentList{
         //region experimental
 
         blockForge = new BlockForge("block-forge"){{
-            requirements(Category.production, BuildVisibility.debugOnly, with(Items.thorium, 100));
+            requirements(Category.production, with(Items.thorium, 100));
             hasPower = true;
             consumes.power(2f);
             size = 3;
         }};
 
         blockLoader = new BlockLoader("block-loader"){{
-            requirements(Category.production, BuildVisibility.debugOnly, with(Items.thorium, 100));
+            requirements(Category.production, with(Items.thorium, 100));
             hasPower = true;
             consumes.power(2f);
             size = 3;
         }};
 
         blockUnloader = new BlockUnloader("block-unloader"){{
-            requirements(Category.production, BuildVisibility.debugOnly, with(Items.thorium, 100));
+            requirements(Category.production, with(Items.thorium, 100));
             hasPower = true;
             consumes.power(2f);
             size = 3;
